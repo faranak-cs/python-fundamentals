ecb7ba04e256ed9e1cfac19df685444e38fdcb51
Faran Ahmad Khan <73027299+faranak-cs@users.noreply.github.com>
2022-12-04 18:41:51 +0500

    If and Else

---
+++ if.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+name = raw_input("What is your name")
+
+print "Your name is " + name
+
+if name == "faran" :
+    print "You are Admin"
+
+elif name == "ahmad" :
+    print "You are user"
+
+
+else:
+    print "unknown"
