30f9267841b4600d4ffed2cc84fabf69fa969753
Faran Khan <73027299+faranak-cs@users.noreply.github.com>
2024-09-16 12:40:52 +0100

    Update httpserver.py (v3.12)

--- httpserver.py
+++ httpserver.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import SimpleHTTPServer, SocketServer
+import http.server
+import socketserver
 
+PORT = 8000
 
-class httpRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler) :
+class httpRequestHandler(http.server.SimpleHTTPRequestHandler) :
     def do_GET(self):
         if self.path == '/admin':
             self.wfile.write("This is only for ADMINS")
             self.wfile.write(self.headers)
 
         else:
-            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+            http.server.SimpleHTTPRequestHandler.do_GET(self)
 
-httpserver = SocketServer.TCPServer(('', 10001), httpRequestHandler)
+httpserver = socketserver.TCPServer(('', PORT), httpRequestHandler)
 
+print("Server started at port ", PORT)
 httpserver.serve_forever()
-
-
-
